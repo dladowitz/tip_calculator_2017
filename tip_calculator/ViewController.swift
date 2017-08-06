860bb6d9270f14516c116f180a4818e4e5c8d7d4
David Ladowitz <david@ladowitz.com>
2017-08-05 18:25:44 -0700

    Save bill amount for 10 min

--- tip_calculator/ViewController.swift
+++ tip_calculator/ViewController.swift
@@ -15,14 +15,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipPercent: UISegmentedControl!
     @IBOutlet weak var totalViewContainer: UIView!
     @IBOutlet var mainViewContainer: UIView!
+    var tipAmounts:Array<Double> = [0.18, 0.20, 0.25]
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
         print("ViewController view viewDidLoad")
-
-
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -31,7 +31,19 @@ class ViewController: UIViewController {
 
         let defaults = UserDefaults.standard
         let defualtTipPercentageIndex = defaults.integer(forKey: "defaultTipPercentageIndex")
+        let lastBillTime = defaults.integer(forKey: "lastBillTime")
+        let currentTimeInMiliseconds = Int(Date().timeIntervalSince1970)
+
         tipPercent.selectedSegmentIndex = defualtTipPercentageIndex
+
+//        Use the last bill amount if it was set less than 10 minutes ago
+        print("Last Bill Time: ", lastBillTime)
+        print("Current Time:   ", currentTimeInMiliseconds)
+        print("Differance:     ", (currentTimeInMiliseconds - lastBillTime))
+        if ((lastBillTime + 600) > currentTimeInMiliseconds){
+            let lastBillAmount = defaults.object(forKey: "lastBillAmount") as! String?
+            billField.text = lastBillAmount
+        }
 
         calculateTip(Any.self)
     }
@@ -79,8 +91,11 @@ class ViewController: UIViewController {
         }
     }
 
+    @IBAction func finishEditingBillField(_ sender: Any) {
+        print("finish editing bill field")
+    }
+
     @IBAction func calculateTip(_ sender: Any) {
-        let tipAmounts = [0.18, 0.20, 0.25]
         let bill = Double(billField.text!) ?? 0
         let selectedTipAmount = Double(tipAmounts[tipPercent.selectedSegmentIndex])
         let tip  = bill * selectedTipAmount
@@ -88,6 +103,14 @@ class ViewController: UIViewController {
 
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+
+//        Save bill to UserDefaults
+        let currentTimeInMiliseconds = Date().timeIntervalSince1970
+        let defaults = UserDefaults.standard
+
+        defaults.set(billField.text, forKey: "lastBillAmount")
+        defaults.set(currentTimeInMiliseconds, forKey: "lastBillTime")
+        defaults.synchronize()
 
     }
 }
